df38369cb5b099ceb959859e4877a54e8f3259e2
Anthony <anthony.tenorio.espinoza@utelvt.edu.ec>
2022-09-28 16:42:10 -0500

    version1

--- compara.cpp
+++ compara.cpp
@@ -1,22 +1,25 @@
 #include<iostream>
 using namespace std;
-
 int main()
 {
-	int AJTE_i=0,AJTE_l;
-	float AJTE_x,AJTE_s=0;
-	cout<<"ingrese el limite AJTE_l="; cin>>AJTE_l;
-	do{
-		
-	cout<<"ingrese el numero AJTE_x="; cin>>AJTE_x;
-	AJTE_i=AJTE_i+1;
-	AJTE_s=AJTE_s+AJTE_x;
+	float a,b;
+	cout<<"ingrese a b:"; cin>>a>>b;
+	if(a>b)
+	{
+	  cout<<a<<" es mayor a "<<b<<endl;
 
 
+	}else{
 
-	}while(AJTE_i<AJTE_l);
-	cout<<"Se ingresaron "<<AJTE_l<<" numeros "<< "que sumaron "<<AJTE_s<<endl;
+	  cout<<b<<" es mayor a "<<a<<endl;
+
+
+	}
+
 	return 0;
+
+
+
 
 
 
